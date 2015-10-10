8085f258235ea102574d0affdb0c02d914ecc8f2
Aditya A Prasad <adityaprasadiskool@gmail.com>
2015-10-11 00:00:45 +0530

    Add README, polish Insertion Sort
    I added the README.md file, and noted the sorting techniques which
    I will be implementing.
    
    I added the comment to show what should be changed to make the
    insertionSort function to sort the list in Descending order.
    
    Signed-off-by: Aditya A Prasad <adityaprasadiskool@gmail.com>

---
+++ README.md
@@ -0,0 +1,6 @@
+##IC 303 DATA STRUCTURES AND ALGORITHMS
+
+*Sorting Techniques
+	Insertion sort, Merge sort, Heap sort, Quick sort, Counting sort, Radix sort, Bucket sort
+	The algorithms given above for sorting are implemented.
+
--- sorting.py
+++ sorting.py
@@ -1,12 +1,24 @@
-# Various sorting techniques - ascending
+# Various sorting techniques - Ascending Order
+
+# Insertion sort, Merge sort, Heap sort,
+# Quick sort, Counting sort, Radix sort, Bucket sort
 
 
 '''
-its In-Place => requires O(1) extra memory
+* Its In-Place => requires O(1) extra memory
+* Efficient for (quite) small data sets
+(The size of list for which insertion sort has the
+advantage varies by environment and implementation,
+but is typically between eight and twenty elements.)
 
+This is like sorting cards, you take the first
+card and seperate it. Then we move on the next
+card and keep it sorted with respect to the
+second card then the third card with
+respect to the first 2, and SO ON.
 '''
 def insertionSort(a):
-    print("\nperforming Insertion Sort on the following list of numbers\n")
+    print("\n Performing Insertion Sort on the following list of numbers \n")
     print(a)
     # we start from 1 as the first element by itself is trivially sorted
     for i in range(1,len(a)):
@@ -14,6 +26,9 @@ def insertionSort(a):
         key = a[i]
         # insert key into its position in the sorted part (a[0] to a[i-1])
         j = i - 1
+        '''
+replace key<a[j] with key>a[j] for Descending
+        '''
         while(j>=0 and key<a[j]):
             # as key is smaller than the j'th element we move towards a[0]
             # switching the elements as we traverse
@@ -29,4 +44,4 @@ def insertionSort(a):
         print("\nThe list is now ->")
         print(a)
         
-insertionSort([1,9,8,7,6,5,4,3,2,1,1,1,0,0.3,0.6,-3,-999])
+insertionSort([1,9,8,7,6,5.9997,4.9999,3,3.00001,2.9999999999999,1,1,0,0.3,0.6,-3,-999])
