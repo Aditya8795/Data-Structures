cca4031a248c34986a174be872324068e190151c
Aditya A Prasad <adityaprasadiskool@gmail.com>
2015-10-11 00:57:46 +0530

    implemented Merge Sort
    But the results are not correct, i seem to have made some mistake in its implementation.
    
    I have to sleep now, will look into it later.
    
    Signed-off-by: Aditya A Prasad <adityaprasadiskool@gmail.com>

--- sorting.py
+++ sorting.py
@@ -3,6 +3,7 @@
 # Insertion sort, Merge sort, Heap sort,
 # Quick sort, Counting sort, Radix sort, Bucket sort
 
+test = [1,9,8,7,6,5.9997,4.9999,3,3.00001,2.9999999999999,1,1,0,0.3,0.6,-3,-999]
 
 '''
 * Its In-Place => requires O(1) extra memory
@@ -44,4 +45,51 @@ replace key<a[j] with key>a[j] for Descending
         print("\nThe list is now ->")
         print(a)
         
-insertionSort([1,9,8,7,6,5.9997,4.9999,3,3.00001,2.9999999999999,1,1,0,0.3,0.6,-3,-999])
+#insertionSort(test)
+
+# Given sorted a[i...j-1] and a[j...k], this function merges both into sorted a[i...k]
+def merge(a,i,j,k):
+    temp = []
+    p = i
+    q = j
+    # we compare the least of both sorted lists and create the merged list 'temp'
+    while(p<j and q<(k+1)):
+        if(a[p]<a[q]):
+            temp.append(a[p])
+            p = p + 1
+        else:
+            temp.append(a[q])
+            q = q + 1
+            
+    # once we have done with a minimum of 1 of the 2 sorted lists we append the remaining list onto the now merged list 'temp'
+    if(p==j):
+        while(q<(k+1)):
+            temp.append(a[q])
+            q = q + 1
+    elif(q==(k+1)):
+        while(p<j):
+            temp.append(a[p])
+            p = p + 1
+            
+    # the merged list 'temp' is used to overwrite the existing list 'a'
+    for l in range(len(temp)):
+        a[i+l] = temp[l]
+
+# test for merge
+#merge([1,2,3,4,5,-3,-2,-1,0],0,5,8)
+
+def mergeSort(a,p,r):
+    # a singleton list is trivially sorted
+    if(p>=r):
+        return
+    else:
+        # // => integer division
+        q = (p+r)//2
+        mergeSort(a, p, q)
+        mergeSort(a, q+1, r)
+        merge(a, p, q, r)
+
+print(test)
+mergeSort(test, 0, (len(test)-1))
+print(test)
+
